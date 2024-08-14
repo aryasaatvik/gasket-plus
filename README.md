a6fae7227e1ce3c0e780f94d61d11d9b5ab7fb23
Saatvik Arya <aryasaatvik@gmail.com>
2024-08-14 14:07:12 -0700

    :tada: Created new repository with gasket create

---
+++ README.md
@@ -0,0 +1,13 @@
+# gasket-nextjs
+
+A basic gasket app
+
+## Setup
+
+```bash
+cd gasket-nextjs
+
+npm install
+
+npx gasket local
+```
---
+++ gasket.js
@@ -0,0 +1,33 @@
+import { makeGasket } from '@gasket/core';
+import pluginHttps from '@gasket/plugin-https';
+import pluginNextjs from '@gasket/plugin-nextjs';
+import pluginWebpack from '@gasket/plugin-webpack';
+import pluginWinston from '@gasket/plugin-winston';
+import pluginLogger from '@gasket/plugin-logger';
+import pluginMetadata from '@gasket/plugin-metadata';
+import pluginCommand from '@gasket/plugin-command';
+import pluginDocs from '@gasket/plugin-docs';
+import pluginDocusaurus from '@gasket/plugin-docusaurus';
+import pluginIntl from '@gasket/plugin-intl';
+
+export default makeGasket({
+  plugins: [
+		pluginHttps,
+		pluginNextjs,
+		pluginWebpack,
+		pluginWinston,
+		pluginLogger,
+		pluginMetadata,
+		pluginCommand,
+		pluginDocs,
+		pluginDocusaurus,
+		pluginIntl
+  ],
+  intl: {
+    locales: [
+      'en-US',
+      'fr-FR',
+    ],
+  },
+  filename: import.meta.filename,
+});
