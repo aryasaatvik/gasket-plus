90b8214ebacd76973e22500fc72c5ca8fdb362dc
Saatvik Arya <aryasaatvik@gmail.com>
2024-10-05 01:39:11 -0700

    custom domain

--- sst.config.ts
+++ sst.config.ts
@@ -1,16 +1,25 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
 export default $config({
   app(input) {
     return {
       name: "gasket-nextjs",
       removal: input?.stage === "prod" ? "retain" : "remove",
       home: "aws",
+      providers: {
+        cloudflare: {
+          version: "5.40.0",
+          apiToken: process.env.CLOUDFLARE_API_TOKEN,
+        },
+      },
     };
   },
   async run() {
     new sst.aws.Nextjs("GasketNextJS", {
-      buildCommand: "pnpm run build:open-next"
+      buildCommand: "pnpm run build:open-next",
+      domain: {
+        name: "gasket.arya.sh",
+        dns: sst.cloudflare.dns()
+      }
     });
   },
 });
