e9db2fc7aac4298dd55aacc28d659f8a8c50ea45
Saatvik Arya <aryasaatvik@gmail.com>
2024-10-07 00:10:04 -0700

    tailwind plugin

--- packages/plugin-tailwind/src/index.ts
+++ packages/plugin-tailwind/src/index.ts
@@ -1,6 +1,11 @@
 import type { Plugin } from '@gasket/core';
-import { name, version, description, devDependencies } from '../package.json';
-import 'create-gasket-app';
+import pkg from '../package.json' assert { type: 'json' };
+import type { CreateContext } from 'create-gasket-app';
+import type { Gasket } from '@gasket/core';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const { name, version, description, devDependencies } = pkg;
 
 const pluginTailwind: Plugin = {
   name,
@@ -12,14 +17,15 @@ const pluginTailwind: Plugin = {
         last: true,
         after: ['@gasket/plugin-nextjs']
       },
-      handler: async function create(gasket, context) {
+      handler: async function create(gasket: Gasket, context: CreateContext) {
         const { files, pkg } = context;
         pkg.add('devDependencies', {
           tailwindcss: devDependencies.tailwindcss,
           postcss: devDependencies.postcss
         });
 
-        const generatorDir = `${__dirname}/../generator`;
+        const __dirname = fileURLToPath(import.meta.url);
+        const generatorDir = path.join(__dirname, '..', '..', 'generator');
 
         files.add(
           `${generatorDir}/*`,
